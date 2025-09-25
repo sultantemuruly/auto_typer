c126eb80ad81a5897889926e356b587435e6f4ad
sultantemuruly <sultantemuruly@gmail.com>
2025-09-25 19:24:10 +0500

    update

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import pyautogui
 import time
 
 
-def auto_writer(text, delay=0.1):
+def auto_typer(file_path, delay=0.5):
+    with open(file_path, "r", encoding="utf-8") as f:
+        text = f.read()
+
     print("You have 5 seconds to focus on the input field...")
     time.sleep(5)
+
     pyautogui.write(text, interval=delay)
+    pyautogui.press("enter")
 
 
 if __name__ == "__main__":
-    my_text = "Some random text to type....."
-    auto_writer(my_text, delay=1)
+    auto_typer("text_to_type.md", delay=0.5)
